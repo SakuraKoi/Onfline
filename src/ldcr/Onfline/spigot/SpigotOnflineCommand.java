af15729455e970418f6d3b9b3696e32a8ce93211
Ldcr <starsky2013@126.com>
2018-05-30 15:23:47 +0800

    Use LdcrUtils CommandHandler as command base

--- src/ldcr/Onfline/spigot/SpigotOnflineCommand.java
+++ src/ldcr/Onfline/spigot/SpigotOnflineCommand.java
@@ -1,25 +1,25 @@
 package ldcr.Onfline.spigot;
 
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-public class SpigotOnflineCommand implements CommandExecutor {
+import ldcr.Utils.Bukkit.command.CommandHandler;
+
+public class SpigotOnflineCommand extends CommandHandler {
+
+	public SpigotOnflineCommand() {
+		super(OnflineSpigot.instance, "§a§lOnfline");
+	}
 
 	@Override
-	public boolean onCommand(final CommandSender sender, final Command command, final String label, final String[] args) {
+	public void onCommand(final CommandSender sender, final String[] args) {
 		if (!(sender instanceof Player)) {
-			sender.sendMessage("§a§lOnfline §7>> §c仅玩家可以进行正版认证!");
-			return true;
+			sendMessage(sender, "§c仅玩家可以进行正版认证!");
+			return;
 		}
-		if (!sender.hasPermission("onfline.use")) {
-			sender.sendMessage("§a§lOnfline §7>> §c你没有权限执行此命令");
-			return true;
-		}
+		if (checkPermission(sender, "onfline.use")) return;
 		OnflineSpigot.instance.messageChannel.requestAuth((Player) sender);
 		OnflineSpigot.log("&e玩家 "+sender.getName()+" 请求进行正版验证");
-		return true;
 	}
 
 }
